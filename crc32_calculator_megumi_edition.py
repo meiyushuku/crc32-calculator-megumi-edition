f56b39218814919640117536a5d4e879b065b2b8
meiyushuku <78764898+meiyushuku@users.noreply.github.com>
2021-02-11 08:07:10 +0800

    Update crc32_calculator_megumi_edition.py

--- crc32_calculator_megumi_edition.py
+++ crc32_calculator_megumi_edition.py
@@ -12,7 +12,7 @@ version = str("1.3.05")
 
 file_name_exclude = [".gitattributes", ".gitignore", "desktop.ini", "thumbs.db"]
 file_ext_exclude = [".py"]
-# For file_filter_list_dir() and file_filter_walk().
+# For file_filter_list_dir(), file_filter_walk().
 
 def start():
     try:
@@ -43,12 +43,12 @@ def menu():
     print("#             .....   :+**#-.::...  ....++*#+*-.          # ")
     print("#             .  .    .=**#*=.....     :***+**:.          # ")
     print("#           ..        :=*++*#+.  .::  :**++-++:.          # ")
-    print("#          ...        .=+=-=***-: . .:**+=+-==.           # Use the rapid command below to start a task.")
+    print("#          ...        .=+=-=***-: . .:**+=+-==.           # Use the rapid command below to start a task:")
     print("#          ..          :=:--======:::.--.====:            # ")
     print("#         ..            ==-=-::.--::..:...::.             # cal -v: Vanish mode.")
-    print("#        -:.   .    .:=****+:...::.....   :==:            # cal -r: Create report as txt file with readability.")
-    print("#        +*=:-+=:-=*#******=.......       =+**+:          # cal -e: Create report as csv file for Excel.")
-    print("#       -*******##*********=.   ::..     :+*****=         # cal -b: Both create.")
+    print("#        -:.   .    .:=****+:...::.....   :==:            # cal -t: Create report as text file with readability.")
+    print("#        +*=:-+=:-=*#******=.......       =+**+:          # cal -c: Create report as CSV file for Microsoft Excel.")
+    print("#       -*******##*********=.   ::..     :+*****=         # cal -b: Create the text and CSV file both.")
     print("#       +******##########***-::-+=-=:.: :+++*****         # ")
     print("#       -*****##########**+*+=:-+:+..-+=*+++*****         # help: View details.")
     print("#                                                         # ")
@@ -72,25 +72,70 @@ def menu():
     #print("          :=+=*+**##*+--: :.:   ..=**-+#+::--*+++=         ")
     mode_switch()
 
-def info():
-    print("Testing...\nPress enter or type any to return menu.")
-    input()
+def info_page1():
+    print("")
+    print("Execution modes below are porvided:")
+    print("")
+    print("Scan modes")
+    print("I. Current directory only")
+    print("II. Tree traversal (including all sub folders)")
+    print("")
+    print("Output modes")
+    print("1. Vanish mode: the calculation result of CRC-32 are only displayed on screen.")
+    print("2. Text file: create report as text file with readability.")
+    print("3. CSV file: create report as CSV file for Microsoft Excel.")
+    print("4. Both: create the text and CSV file both.")
+    print("")
+    print("The folder named crc_report will be created (if none) in current directory and reports will be saved to there.")
+    print("The report file will be named with system time converted to UTC+0 in ISO 8601 format.")
+    print("")
+    print("Files and folders below have been excluded by default:")
+    print("")
+    print("desktop.ini")
+    print("thumbs.db")
+    print(".gitattributes")
+    print(".gitignore")
+    print(".py")
+    print(".git")
+    print("crc_report")
+    print("")
+    print("You can also add custom file extension exclusions with a command.")
+    print("")
+    input("Next >>> ")
+    info_page2()
+
+def info_page2():
+    print("")
+    print("Command list:")
+    print("")
+    print("cal -v: Scan with mode I and output with mode 1.")
+    print("cal -t: Scan with mode I and output with mode 2.")
+    print("cal -c: Scan with mode I and output with mode 3.")
+    print("cal -b: Scan with mode I and output with mode 4.")
+    print("cal --walk-v: Scan with mode II and output with mode 1.")
+    print("cal --walk-t: Scan with mode II and output with mode 2.")
+    print("cal --walk-c: Scan with mode II and output with mode 3.")
+    print("cal --walk-b: Scan with mode II and output with mode 4.")
+    print("add -ext: Add custom file extension exclusions.")
+    print("")
+    input("Menu >>> ")
+    print("")
     return start()
 
 def mode_switch():                                                                     
     try:
-        user_input_menu_ori = input("########################################################### Type here: ")
+        user_input_menu_ori = input("########################################################### Type here >>> ")
         global scan_mission, output_mission
         user_input_menu = user_input_menu_ori.strip()
         if user_input_menu == "cal -v":
             scan_mission = 100
             output_mission = 60
             return
-        elif user_input_menu == "cal -r":
+        elif user_input_menu == "cal -t":
             scan_mission = 100
             output_mission = 70
             return
-        elif user_input_menu == "cal -e":
+        elif user_input_menu == "cal -c":
             scan_mission = 100
             output_mission = 80
             return
@@ -102,11 +147,11 @@ def mode_switch():
             scan_mission = 200
             output_mission = 60
             return
-        elif user_input_menu == "cal --walk-r":
+        elif user_input_menu == "cal --walk-t":
             scan_mission = 200
             output_mission = 70
             return
-        elif user_input_menu == "cal --walk-e":
+        elif user_input_menu == "cal --walk-c":
             scan_mission = 200
             output_mission = 80
             return
@@ -115,7 +160,7 @@ def mode_switch():
             output_mission = 90
             return
         elif user_input_menu == "help":
-            info()
+            info_page1()
         elif user_input_menu == "":
             print("########################################################### Not entered.")
             return mode_switch()
@@ -358,7 +403,11 @@ def outputer_csv():
         report_folder = ".\\crc_report"
         if not os.path.isdir(report_folder):
             os.mkdir(report_folder)
-        writer = codecs.open(".\\crc_report\\" + csv_name + ".csv", "a","utf-8")
+        if file_count == 1:
+            writer = codecs.open(".\\crc_report\\" + csv_name + ".csv", "a","utf-8")
+            writer.write("Path,File,Size,Value,Timestamp\n")
+        else:
+            writer = codecs.open(".\\crc_report\\" + csv_name + ".csv", "a","utf-8")
         if file_count != file_count_total:
             writer.write("{:s},{:s},{:s},{:s},{:s}\n".format(
                 csv_list[0],
@@ -408,24 +457,27 @@ while controller: # Mean: while controller != 0
         file_filter_walk()
     for file_name in file_name_list:
         file_name_abs = os.path.abspath(file_name)
+        # For display() and outputer_?().
         file_size = os.path.getsize(file_name)
         # For file_size_filter(), display() and crc_core().
         file_size_class = file_size_filter(file_size)
         file_count += 1
         display()
-        # Global var: file_name_abs, file_size, file_size_class
+        # Global var below:
+        # file_name, file_name_abs, file_size, file_size_class
     ##################################################################
     ### Next: display() -> crc_core() -> display() -> outputer_?() ###
     ##################################################################
 
     if file_count == 0:
-        print("No file in this folder. Use the command below to do something or exit by any other press.")
+        print("No file which need to be calculated in this folder.")
+        print("Use the command below to do something or exit by any other.")
     else:
-        print("Task is completed. Use the command below to do something or exit by any other press.")
+        print("Task is completed. Use the command below to do something or exit by any other.")
     print("")
     print("menu: Return to menu.")
     print("")
-    user_input_end_ori = input("Type here: ")
+    user_input_end_ori = input("Type here >>> ")
     user_input_end = user_input_end_ori.strip()
     if user_input_end == "menu":
         pass
